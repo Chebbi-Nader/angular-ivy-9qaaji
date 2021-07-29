e0dff9c046423e0205fdc25376311a030b9163e3
Nadirateur <nader.chebbi@outlook.com>
2021-07-29 12:00:22 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AlbumComponent } from './album/album/album.component';
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, AlbumComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
