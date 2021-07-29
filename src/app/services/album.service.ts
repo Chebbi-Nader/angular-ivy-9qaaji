84ec72ff85fb983fe60b90d96fda133945add124
Nadirateur <nader.chebbi@outlook.com>
2021-07-29 14:46:15 +0200

    2 files modified

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/dist/types';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AlbumService {
   private _apiUrl = 'https://jsonplaceholder.typicode.com/albums';
   constructor(private _httpClient: HttpClient) {}
 
-  getAllAlbum(): Observable<Object[]> {
+  getAllAlbum(): Observable<any> {
     return this._httpClient.get(this._apiUrl);
   }
 }
